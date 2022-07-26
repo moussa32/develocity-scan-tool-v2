88a62feab2f1e249df9d1f33733453f19404c6e6
reham90 <rehamadel344@gmail.com>
2022-07-26 17:24:05 +0300

    new commit

--- src/components/ChangelogContent/ChangelogContent.js
+++ src/components/ChangelogContent/ChangelogContent.js
@@ -35,27 +35,27 @@ const ChangelogContent =() =>{
   <li className={`${styles.tabLink}  nav-item`} role="presentation">
     <button 
     onClick={changecategory} value="All"
-    className={`${styles.all} ${styles.navLink} nav-link active`}   type="button" role="tab" aria-controls="pills-all" aria-selected="true">{t("changelog:view")}</button>
+    className={`${category=="All" && styles.btnAll} ${styles.navLink} nav-link `}   type="button" role="tab" aria-controls="pills-all" aria-selected="true">{t("changelog:view")}</button>
   </li>
   <li className={`${styles.tabLink}  nav-item`} role="presentation">
     <button
     onClick={changecategory}  value="Announcement"
-    className={`${styles.announcement} ${styles.navLink} nav-link`}  type="button" role="tab" aria-controls="pills-announcement" aria-selected="false">{t("changelog:announce")}</button>
+    className={`${category=="Announcement" && styles.btnAnnounce} ${styles.navLink} nav-link`}  type="button" role="tab" aria-controls="pills-announcement" aria-selected="false">{t("changelog:announce")}</button>
   </li>
   <li className={`${styles.tabLink}  nav-item`} role="presentation">
     <button
      onClick={changecategory} value="Bug Fix"
-    className={`${styles.bugFix} ${styles.navLink} nav-link`}  type="button" role="tab" aria-controls="pills-bugFix" aria-selected="false">{t("changelog:bug")}</button>
+    className={`${category=="Bug Fix" && styles.btnBug} ${styles.navLink} nav-link`}  type="button" role="tab" aria-controls="pills-bugFix" aria-selected="false">{t("changelog:bug")}</button>
   </li>
   <li className={`${styles.tabLink}  nav-item`} role="presentation">
   <button 
   onClick={changecategory} value="Product"
-  className={`${styles.tabProduct} ${styles.navLink} nav-link`}  type="button" role="tab" aria-controls="pills-tabProduct" aria-selected="false">{t("changelog:product")}</button>
+  className={`${category=="Product" && styles.btnProduct} ${styles.navLink} nav-link`}  type="button" role="tab" aria-controls="pills-tabProduct" aria-selected="false">{t("changelog:product")}</button>
 </li>
 <li className={`${styles.tabLink}  nav-item`} role="presentation">
 <button 
 onClick={changecategory} value="Company"
-className={`${styles.tabCompany} ${styles.navLink} nav-link`}  type="button" role="tab" aria-controls="pills-tabCompany" aria-selected="false">{t("changelog:company")}</button>
+className={`${category=="Company" && styles.btnCompany} ${styles.navLink} nav-link`}  type="button" role="tab" aria-controls="pills-tabCompany" aria-selected="false">{t("changelog:company")}</button>
 </li>
 </ul>
 <div className={`${styles.tabContent} tab-content`} id="pills-tabContent">
@@ -72,8 +72,11 @@ className={`${styles.tabCompany} ${styles.navLink} nav-link`}  type="button" rol
       `}>
         {el.news.map( (ele=>(
            <div className={styles.typeBlock}>
-           <div className={lang=="ar"?lang=="ar"?styles.typeDate_rtl:styles.typeDate_ltr_rtl:lang=="ar"?styles.typeDate_rtl:styles.typeDate_ltr_ltr}>
-           <h1 className={styles.typeAnnounce}>All</h1>
+           <div className={lang=="ar"?styles.typeDate_rtl:styles.typeDate_ltr}>
+           <h1 className={`${el.category=="announcement" && styles.typeAnnounce}
+          ${el.category=="product" && styles.typeProduct}
+          ${el.category=="bug fix" && styles.typeBug}
+          ${el.category=="company" && styles.typeCompany}`}>{el.category}</h1>
            <p className={styles.date}>{ele.createdAt}</p>
            </div>
            </div>
@@ -101,84 +104,7 @@ className={`${styles.tabCompany} ${styles.navLink} nav-link`}  type="button" rol
 
   {/* end */}
   
-  {/* <div className={`${styles.step} ${styles.announce}`}>
-    <div className={styles.typeBlock}>
-    <div className={lang=="ar"?lang=="ar"?styles.typeDate_rtl:styles.typeDate_ltr_rtl:lang=="ar"?styles.typeDate_rtl:styles.typeDate_ltr_ltr}>
-    <h1 className={styles.typeAnnounce}>All</h1>
-    <p className={styles.date}>8 JULY, 2022</p>
-    </div>
-    </div>
-    <div className={styles.vStepper}>
-      <div className={styles.circle}></div>
-      <div className={styles.line}></div>
-    </div>
 
-    <div className={lang=="ar"?styles.content_rtl:styles.content_ltr}>
-    <h2 className={styles.title}>Whitepaper Got Updated - Go Read!</h2>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-      </p>
-    </div>
-  </div>
-
-
-<div className={`${styles.step} ${styles.product}`}>
-<div className={styles.typeBlock}>
-<div className={lang=="ar"?styles.typeDate_rtl:styles.typeDate_ltr}>
-<h1 className={styles.typeProduct}>product</h1>
-<p className={styles.date}>8 JULY, 2022</p>
-</div>
-</div>
-<div className={styles.vStepper}>
-  <div className={styles.circle}></div>
-  <div className={styles.line}></div>
-</div>
-
-<div className={lang=="ar"?styles.content_rtl:styles.content_ltr}>
-<h2 className={styles.title}>Whitepaper Got Updated - Go Read!</h2>
-  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-  </p>
-</div>
-</div>
-
-
-<div className={`${styles.step} ${styles.bug}`}>
-<div className={styles.typeBlock}>
-<div className={lang=="ar"?styles.typeDate_rtl:styles.typeDate_ltr}>
-<h1 className={styles.typeBug}>bug fix</h1>
-<p className={styles.date}>8 JULY, 2022</p>
-</div>
-</div>
-  <div className={styles.vStepper}>
-    <div className={styles.circle}></div>
-    <div className={styles.line}></div>
-  </div>
-
-  <div className={lang=="ar"?styles.content_rtl:styles.content_ltr}>
-  <h2 className={styles.title}>Whitepaper Got Updated - Go Read!</h2>
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-    </p>
-  </div>
-</div>
-
-
-<div className={`${styles.step} ${styles.company}`}>
-<div className={styles.typeBlock}>
-<div className={lang=="ar"?styles.typeDate_rtl:styles.typeDate_ltr}>
-<h1 className={styles.typeCompany}>company</h1>
-<p className={styles.date}>8 JULY, 2022</p>
-</div>
-</div>
-  <div className={styles.vStepper}>
-    <div className={styles.circle}></div>
-    <div className={styles.line}></div>
-  </div>
-
-  <div className={lang=="ar"?styles.content_rtl:styles.content_ltr}>
-  <h2 className={styles.title}>Whitepaper Got Updated - Go Read!</h2>
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-    </p>
-  </div>
-</div> */}
 
   </div>
 
