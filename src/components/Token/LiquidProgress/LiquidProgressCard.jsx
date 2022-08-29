8acb1c08645522a608323048aca7cf3e6c65080e
Aya Mohy <aya.mohey162@gmail.com>
2022-08-30 01:33:24 +0200

    adding honypot+rugpul+changelog loader & edit ui changelog

--- src/components/Token/LiquidProgress/LiquidProgressCard.jsx
+++ src/components/Token/LiquidProgress/LiquidProgressCard.jsx
@@ -1,22 +1,12 @@
 import React  from "react"
-<<<<<<< HEAD
-import { useState } from "react"
-=======
->>>>>>> 5ed9685130d13e431a50302d86d3e65fab906a19
 import { ProgressCard } from "../ProgressContainerReuse/ProgressCard"
 import { useTranslation } from 'react-i18next';
 
 export function LiquidProgressCard(){
-<<<<<<< HEAD
-  const { t, i18n } = useTranslation(["token"])
-  const lang=localStorage.getItem("i18nextLng")
-    let data={
-        title:'Liquidity',
-=======
+
   const { t } = useTranslation(["token"])
     let data={
         title:t("token:liquidity"),
->>>>>>> 5ed9685130d13e431a50302d86d3e65fab906a19
         percentdecimal:2/5,
         precentageover:-5.24,
         series:40,
