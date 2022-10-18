74e499bb10bf75eeec471c253b783da32c46a89a
Aya Mohy <aya.mohey162@gmail.com>
2022-10-18 10:25:13 +0200

    pulling

--- src/Pages/LinkTree/LinkButton.js
+++ src/Pages/LinkTree/LinkButton.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import styles from './LinkTree.module.css'
+import ShareButton from './ShareButton'
 export default function LinkButton({title}) {
   return (
     <>
     <div className={styles['link-wrapper']}>
-        <span>{title}</span>
+        <h5>{title}</h5>
+        <div className={styles['link-btnShare']}>
+        <ShareButton />
+
+        </div>
     </div>
     </>
   )
