9efa26dd00e2fb1d2f46f4d46653b847a127a2e1
Aya Mohy <aya.mohey162@gmail.com>
2022-11-02 14:06:51 +0200

    fixing

--- src/components/Home/Header/NavBar.jsx
+++ src/components/Home/Header/NavBar.jsx
@@ -1,6 +1,6 @@
 import { NavDisplay } from "../../common/Navbars/headerNavbar/NavDisplay";
 import SocialBar from "../../common/Navbars/SocialBar/SocialBar";
-import { useTranslation } from 'react-i18next';
+// import { useTranslation } from 'react-i18next';
 
 const NavBar =() =>{
     return(
