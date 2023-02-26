f6af865bbd93e4ad9429c07389e46aaf6f430e7f
AbdulazizBinFahad <94074156+AbdulazizBinFahad@users.noreply.github.com>
2023-02-26 06:53:38 +0300

    from test to live

--- src/config/axiosconfig.js
+++ src/config/axiosconfig.js
@@ -1,8 +1,8 @@
 import axios from "axios"; 
 
 const instance = axios.create({
-  // baseURL : 'https://api.develocity.finance/api/v1/',  //live server
-  baseURL : 'http://20.218.124.106:1885/api/v1/',  //test server
+  baseURL : 'https://api.develocity.finance/api/v1/',  //live server
+  // baseURL : 'http://20.218.124.106:1885/api/v1/',  //test server
 //   headers: {
 // //  Authorization: `<Your Auth Token>`,
 //     Content-Type: "application/json",
