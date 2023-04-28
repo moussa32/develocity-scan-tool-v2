f2ec07199b30246680f48d6b8ea5af5c5e6c9667
AbdulazizBinFahad <94074156+AbdulazizBinFahad@users.noreply.github.com>
2023-04-28 15:51:34 +0300

    Update socket.jsx

--- src/config/socket.jsx
+++ src/config/socket.jsx
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 // export const socket = io('http://20.218.124.106:1885');
 
 // live
-export const socket = io("https://api.develocity.finance", {
+export const socket = io("https://api.xite.solutions", {
   extraHeaders: {
     ["my-auth"]: "abcd",
   },
