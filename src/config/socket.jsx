82df69c964355bdd86dc10fffc7080d2b40ec213
AbdulazizBinFahad <94074156+AbdulazizBinFahad@users.noreply.github.com>
2023-05-15 02:02:55 +0300

    .

--- src/config/socket.jsx
+++ src/config/socket.jsx
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 // export const socket = io('http://20.218.124.106:1885');
 
 // live
-export const socket = io("https://api.xite.solutions", {
+export const socket = io("https://develocity-blockchain-stagging-production.up.railway.app/", {
   extraHeaders: {
     ["my-auth"]: "abcd",
   },
