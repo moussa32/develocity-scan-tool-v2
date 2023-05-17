1861fd622cbee833c0fa973109e7c35b7d18991f
AbdulazizBinFahad <94074156+AbdulazizBinFahad@users.noreply.github.com>
2023-05-17 20:28:29 +0300

    Update ResultDropdown.jsx

--- src/components/Home/Search/ResultDropdown.jsx
+++ src/components/Home/Search/ResultDropdown.jsx
@@ -20,19 +20,34 @@ const ResultDropdown = ({ contractAddress, logo, name, symbol, contractScan, isS
             <h6 className={styles.icon_token_text}>{name.charAt(0)}</h6>
           </div>
         )}
+
         <div className={styles.details}>
           <div className={styles.headerTitle}>
             <h2 className={styles.resultTokenName}>{name}</h2>
-            <span className={styles.resultTokenTicker}>{symbol}</span>
+            <span className={styles.resultTokenTicker}>{symbol}&nbsp;&nbsp;</span>
+            {isScam && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="#9F4AE8"
+              viewBox="0 0 16 16"
+              className={`${styles.tokenRecord_verifyIcon} ${
+              styles.tokenRecord_verifyIcon_rtl 
+              }`}
+            >
+              <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" />
+            </svg>
+          )}
           </div>
           <div className={styles.resultTokenDetails}>
             <div className={styles.address}>{contractAddress.slice(0, 8) + "..." + contractAddress.slice(31, 41)}</div>
 
-            {contractScan < 59 && (
+            {contractScan <= 59 && (
               <div className={`${styles.scanCard} ${styles.scanRed}`}>Score:{Math.round(contractScan)}</div>
             )}
 
-            {contractScan > 59 && contractScan < 84 && (
+            {contractScan > 59 && contractScan <= 84 && (
               <div className={`${styles.scanCard} ${styles.scanYellow}`}>Score:{Math.round(contractScan)}</div>
             )}
 
