f9971b2151f03de0f0757dafb1850238edb432c4
AbdulazizBinFahad <94074156+AbdulazizBinFahad@users.noreply.github.com>
2023-05-22 00:50:12 +0300

    .

--- src/components/Home/Search/MySearch.jsx
+++ src/components/Home/Search/MySearch.jsx
@@ -159,7 +159,7 @@ const MySearch = () => {
       <div className={styles.searchNote2}>
         <span className={styles.note}>{t("common:sponsered")}</span>
         <a className={styles.note2} href="https://develocity.finance">
-          <img src="logo.png" className={direction === "rtl" ? styles.bitcoin_rtl : styles.bitcoin_ltr} />
+          <img src="../../logo.png" className={direction === "rtl" ? styles.bitcoin_rtl : styles.bitcoin_ltr} />
           {t("common:invest")}
         </a>
       </div>
