d7c169191dc2bf2f6d8f6c0cd425635f5d23f566
BagAziz <146094948+BagAziz@users.noreply.github.com>
2023-12-13 01:50:53 +0300

    update homepage ui

--- src/pages/home/PopularCard.jsx
+++ src/pages/home/PopularCard.jsx
@@ -3,28 +3,30 @@ import { scoreTypeColor } from "@/shared/util";
 
 const PopularCard = ({ score, scans, symbol, name, logo }) => {
   return (
-    <div className="bg-[#272B40] xl:px-[42px] rounded-lg p-6 lg:py-8 flex justify-between flex-wrap lg:flex-nowrap items-center">
+    <div className="bg-[#272B40] rounded-lg p-3 flex justify-between flex-wrap lg:flex-nowrap items-center">
       <div className="flex text-white flex-shrink-1 grow-0">
         <img
-          className="ltr:mr-4 rtl:ml-4 w-16 h-16 lg:w-14 lg:h-14 xl:w-[105px] xl:h-[105px]"
-          src={logo}
+          className="ltr:mr-4 rtl:ml-4 w-16 h-16 lg:w-14 lg:h-14 xl:w-[50px] xl:h-[50px]"
+          src={logo ? logo : "/src/assets/images/No_image_available.png"}
           alt={name}
           title={name}
         />
-        <div className="flex flex-col items-start justify-center gap-[2px]">
-          <h2 className="font-bold font-segoe text-xl lg:text-2xl">{name}</h2>
-          <span className="flex-initial font-segoe text-xl lg:text-2xl">
+        <div className="flex flex-col justify-center gap-[1px]">
+          <h2 className="font-bold font-segoe text-md lg:text-md">
+            {name}
+          </h2>
+          <span className="flex-initial font-segoe text-md lg:text-md">
             {symbol}
           </span>
         </div>
       </div>
       <div className="flex mt-6 lg:mt-0 flex-row-reverse justify-between lg:flex-col gap-3 order-3 lg:order-2 items-center w-full">
-        <span className="font-segoe text-white text-xl lg:text-2xl">
+        <span className="font-segoe text-white text-md lg:text-md">
           Scan: {Number(scans).toFixed(0)}
         </span>
-        <div className="bg-[#2F334B] text-[#888888] text-lg flex items-center justify-center py-2 px-[9.1px] rounded-sm gap-[9px]">
+        <div className="bg-[#2F334B] text-[#888888] text-md flex items-center justify-center py-2 px-[7px] rounded-sm gap-[5px]">
           <img
-            className="w-[27.15px] h-[27.15px]"
+            className="w-[20px] h-[20px]"
             src={BSC}
             alt="network"
             title="network"
@@ -32,7 +34,7 @@ const PopularCard = ({ score, scans, symbol, name, logo }) => {
           <span>BSC</span>
         </div>
       </div>
-      {score && (
+      {score ? (
         <span
           className={`flex items-center order-2 lg:order-3 justify-center font-medium text-[26px] pt-[4px] pb-[7px] px-[15px] ${
             scoreTypeColor(score).bg
@@ -40,8 +42,17 @@ const PopularCard = ({ score, scans, symbol, name, logo }) => {
         >
           {Number(score).toFixed(0)}
         </span>
+      ) : (
+        <span
+          className={`flex items-center order-2 lg:order-3 justify-center font-medium text-[26px] pt-[4px] pb-[7px] px-[15px] bg-red bg-danger
+            ${scoreTypeColor(score).text} bg-opacity-20 rounded-sm`}
+        >
+          0
+        </span>
       )}
     </div>
+
+
   );
 };
 
