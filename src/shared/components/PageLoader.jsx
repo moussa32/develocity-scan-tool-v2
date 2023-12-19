73d7911d49e43c61fb581a6bec6443682feb549c
Moussa Ibrahim <moussaebrahem32@gmail.com>
2023-12-19 16:57:33 +0200

    fix: hide video controles

--- src/shared/components/PageLoader.jsx
+++ src/shared/components/PageLoader.jsx
@@ -4,7 +4,7 @@ import LoadingWEBM from "@assets/videos/PageLoader.webm";
 const PageLoader = () => {
   return (
     <section className="flex items-center justify-center min-h-screen bg-[#25293E]">
-      <video muted autoPlay>
+      <video muted playsInline preload="auto" loop autoPlay controls={false}>
         <source src={LoadingWEBM} />
         <source src={LoadingMP4} />
       </video>
